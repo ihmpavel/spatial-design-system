9c034c95180411fbf7afae70b66775ca7c7e821d
ihmpavel <42217494+ihmpavel@users.noreply.github.com>
2025-04-09 22:44:39 +0000

    reafactor: TypeScript types for `utils.js`

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,9 @@
-export { stringifyForHTML } from './utils'
+declare module "spatial-design-system/utils/utils.js" {
+  type Item = {
+    color: string;
+    title: string;
+    textColor: string;
+  }
+
+  export function stringifyForHTML(items: Item[]): string
+}
--- types/utils.d.ts
+++
@@ -1,7 +0,0 @@
-type Item = {
-  color: string;
-  title: string;
-  textColor: string;
-}
-
-export function stringifyForHTML(items: Item[]): string
